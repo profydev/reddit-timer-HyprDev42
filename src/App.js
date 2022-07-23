3459a81985b1c6bd573fc0bdd8bce91e923e811f
HyprDev <hyprdev42@gmail.com>
2022-07-23 14:30:15 -0400

    Fixed issues with setting up react router. Added routes home and search.

--- src/App.js
+++ src/App.js
@@ -1,10 +1,14 @@
 import React from 'react';
+import { Routes, Route, Link } from 'react-router-dom';
+import Home from './components/home';
+import Search from './components/search';
 
 function App() {
   return (
-    <div>
-      App Placeholder
-    </div>
+    <Routes>
+      <Route path="/" element={<Home />} />
+      <Route path="/search" element={<Search />} />
+    </Routes>
   );
 }
 
--- src/index.js
+++ src/index.js
@@ -1,17 +1,13 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import { BrowserRouter } from "react-router-dom";
+import { BrowserRouter as Router } from "react-router-dom";
 import './index.css';
 import App from './App';
 
-const root = ReactDOM.createRoot(
-    document.getElementById("root")
-);
-
-root.render(
-    <React.StrictMode>
-        <BrowserRouter>
-            <App />
-        </BrowserRouter>
-    </React.StrictMode>
+ReactDOM.render(
+    <Router>
+        <App />
+    </Router>
+    ,
+    document.getElementById('root')
 );
