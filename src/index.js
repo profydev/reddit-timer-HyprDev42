f1457d6d0a4f0e3005eaad7efd1d43d2cbb8b701
HyprDev <hyprdev42@gmail.com>
2022-07-23 14:00:40 -0400

    Changed App.css to index.css, Installed react router dom.

--- src/index.js
+++ src/index.js
@@ -1,9 +1,17 @@
-// import global styles
-import './App.css';
-
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { BrowserRouter } from "react-router-dom";
+import './index.css';
 import App from './App';
 
-// eslint-disable-next-line react/jsx-filename-extension
-ReactDOM.render(<App />, document.getElementById('root'));
+const root = ReactDOM.createRoot(
+    document.getElementById("root")
+);
+
+root.render(
+    <React.StrictMode>
+        <BrowserRouter>
+            <App />
+        </BrowserRouter>
+    </React.StrictMode>
+);
