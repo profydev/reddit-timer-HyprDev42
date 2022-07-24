39d2ab690945d494094f39b67aa52ae10f55f7f1
HyprDev <hyprdev42@gmail.com>
2022-07-24 17:03:39 -0400

    Finally, fixed all dependency issues, I think...

--- src/index.js
+++ src/index.js
@@ -1,14 +1,13 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import { BrowserRouter as Router } from "react-router-dom";
+import { BrowserRouter as Router } from 'react-router-dom';
 import './index.css';
 import App from './App';
 import '@csstools/normalize.css';
 
 ReactDOM.render(
-    <Router>
-        <App />
-    </Router>
-    ,
-    document.getElementById('root')
+  <Router>
+    <App />
+  </Router>,
+  document.getElementById('root'),
 );
