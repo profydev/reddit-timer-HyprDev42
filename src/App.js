935813a7792ba09fe8ae299c8c966b19b692b2b9
HyprDev <hyprdev42@gmail.com>
2022-07-24 17:54:02 -0400

    Fixed issues with formatting.

--- src/App.js
+++ src/App.js
@@ -3,13 +3,13 @@ import { Routes, Route } from 'react-router-dom';
 import Home from './components/home';
 import Search from './components/search';
 
-function App() {
+const App = () => {
   return (
     <Routes>
       <Route path="/" element={<Home />} />
       <Route path="/search" element={<Search />} />
     </Routes>
   );
-}
+};
 
 export default App;
--- src/components/search.jsx
+++ src/components/search.jsx
@@ -1,11 +1,11 @@
 import React from 'react';
 
 const Search = () => {
-    return (
-        <div>
-            <h1>Search Page</h1>
-        </div>
-    );
-}
+  return (
+    <div>
+      <h1>Search Page</h1>
+    </div>
+  );
+};
 
 export default Search;
