bd3cb502413915d5816079cfa218c1d1c2a54e51
HyprDev <hyprdev42@gmail.com>
2022-07-24 19:56:59 -0400

    Put the browser router inside the app.js file to fix issues with CI tests.

--- src/App.js
+++ src/App.js
@@ -1,17 +1,17 @@
 import React from 'react';
-import { Routes, Route } from 'react-router-dom';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import Home from './components/home';
 import Search from './components/search';
 
 const App = () => {
   return (
-    <>
+    <BrowserRouter>
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/search" element={<Search />} />
         <Route path="*" element={<div>404</div>} />
       </Routes>
-    </>
+    </BrowserRouter>
   );
 };
 
--- src/index.js
+++ src/index.js
@@ -1,16 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import { BrowserRouter as Router } from 'react-router-dom';
 import './index.css';
 import App from './App';
 import '@csstools/normalize.css';
 
-const element = (
-  <Router>
-    <App />
-  </Router>
-);
-
-const container = document.getElementById('root');
-
-ReactDOM.render(element, container);
+ReactDOM.render(<App />, document.getElementById('root'));
