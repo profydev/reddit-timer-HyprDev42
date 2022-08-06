bce4dd3b9e08a92bc40cd936e5b87afd43a33061
HyprDev42 <108822303+HyprDev42@users.noreply.github.com>
2022-08-05 21:08:55 -0400

    Create app skeleton (#1)
    * Added Google Fonts - Bitter and Montserrat and their needed font weights.
    
    * Added normalize.css to project.
    
    * Fixed some dependency issues. Added global styles import into index.js, created components folder for future use.
    
    * Added global styles to App.css file.
    
    * Changed App.css to index.css, Installed react router dom.
    
    * Fixed issues with setting up react router. Added routes home and search.
    
    * Fixed lint dependency issue.
    
    * Fixed lint react hooks dependency issue.
    
    * Fix dependency issues again.
    
    * Fixed dependency issues, also added normalize.css import to index.js
    
    * Finally, fixed all dependency issues, I think...
    
    * Fixed issues with formatting.
    
    * Fixed issues with browser router.
    
    * Fixed issues with browser router. Added extra catch-all route.
    
    * Put the browser router inside the app.js file to fix issues with CI tests.

--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import './index.css';
 import App from './App';
+import '@csstools/normalize.css';
 
-// eslint-disable-next-line react/jsx-filename-extension
 ReactDOM.render(<App />, document.getElementById('root'));
